eeec7c0c49a86778835f1b81bbba68ea396c0e2d
johnsey <johnsey.reynolds@gmail.com>
2022-06-04 16:28:20 -0400

    Assignment Completed

--- src/data/user.js
+++ src/data/user.js
@@ -4,3 +4,17 @@ const city = "New York";
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
 // export all three variables as named exports
+
+export { username, city, image };
+
+
+
+// import { username, city, image } from "../data/user";
+
+// test("username, city, and image are all exported", () => {
+// expect(username).toEqual("Liza");
+//   expect(city).toEqual("New York");
+//   expect(image).toEqual("https://i.imgur.com/mV8PQxj.gif");
+// });
+
+
